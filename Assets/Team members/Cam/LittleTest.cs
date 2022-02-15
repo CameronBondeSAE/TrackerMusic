842099a7a2bbed0b03067c653a19231b4ca98cc3
CameronBondeSAE <c.bonde@sae.edu>
2022-02-15 15:15:52 +1100

    Minimal example script

---
+++ Assets/Team members/Cam/LittleTest.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using SharpMik;
+using SharpMik.Player;
+using UnityEngine;
+
+public class LittleTest : MonoBehaviour
+{
+    // The music player
+    public SharpMikManager sharpMikManager;
+    
+    void Start()
+    {
+        // Subscribing to C# Event when a note plays
+        ModPlayer.NoteEvent += ModPlayerOnNoteEvent;
+
+        // GPG230 stuff
+        UnityThread.initUnityThread();
+    }
+
+    // GPG230 stuff
+    private void ModPlayerOnNoteEvent(MP_CONTROL mpcontrol)
+    {
+        UnityThread.executeInUpdate(() =>
+        {
+            NotePlayedEvent(mpcontrol);
+        });
+    }
+
+    private void NotePlayedEvent(MP_CONTROL newNotePlayed)
+    {
+        // Your code goes here
+        Debug.Log(newNotePlayed.anote + " : Vol = "+newNotePlayed.volume);
+    }
+}
