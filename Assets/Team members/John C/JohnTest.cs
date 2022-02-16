a763ca9013b89abc35de13dcf0c5f7440b5241b8
Cam <c.bonde@sae.edu>
2022-02-17 00:14:24 +1100

    Removed unused 'using' preventing play mode

--- Assets/Team members/John C/JohnTest.cs
+++ Assets/Team members/John C/JohnTest.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using SharpMik;
 using SharpMik.Player;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
