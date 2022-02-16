2959e65ec9129b47813b0d2b5dfa0bd39eff60d0
JohnnyBoii909 <1018518@student.sae.edu.au>
2022-02-16 17:04:26 +1100

    John's Test Evironment Push

--- Assets/Team members/John C/JohnTest.cs
+++ Assets/Team members/John C/JohnTest.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using SharpMik;
 using SharpMik.Player;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class JohnTest : MonoBehaviour
 {
     // The music player
     public SharpMikManager sharpMikManager;
-    
+    public GameObject cubeObject;
+    public Color newColour;
+
+
     void Start()
     {
         // Subscribing to C# Event when a note plays
@@ -19,16 +25,27 @@ public class JohnTest : MonoBehaviour
     }
 
     // GPG230 stuff
-    private void ModPlayerOnNoteEvent(MP_CONTROL mpcontrol)
+    private void ModPlayerOnNoteEvent(MP_CONTROL mpControl)
     {
         UnityThread.executeInUpdate(() =>
         {
-            NotePlayedEvent(mpcontrol);
+            NotePlayedEvent(mpControl);
         });
     }
 
     private void NotePlayedEvent(MP_CONTROL newNotePlayed)
     {
-        // Your code goes here
+        GameObject go = Instantiate(cubeObject,Vector3.zero, Quaternion.identity);
+        go.GetComponent<CubeLogic>().note = newNotePlayed.anote;
+        go.GetComponent<Renderer>().material.color = newColour;
+
+
     }
+
+    void Update()
+    {
+        // pick a random color.
+        newColour = new Color( Random.value, Random.value, Random.value, 1.0f );
+    }
+    
 }
