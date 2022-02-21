a4ecddf378a272af8a39382ebf58155809c6cb45
Valentine <myownvalentine667@gmail.com>
2022-02-21 15:33:09 +1100

    added a little laser light show in the background

--- Assets/Team members/Maya/Scripts/MayaStuff.cs
+++ Assets/Team members/Maya/Scripts/MayaStuff.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using SharpMik;
 using SharpMik.Player;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
@@ -16,7 +17,11 @@ public class MayaStuff : MonoBehaviour
     //3d stuff
     private GameObject parent;
     private GameObject left;
-    private GameObject right;
+    private GameObject right;    
+    private GameObject cubeParent;
+    private GameObject cubeLeft;
+    private GameObject cubeRight;
+    private Vector3 defaultCoreScale;
     public List<GameObject> shapes;
     public Object[] materials;
     public GameObject core;
@@ -27,7 +32,10 @@ public class MayaStuff : MonoBehaviour
 
     void Start()
     {
-        //lights
+        //3d stuff
+        defaultCoreScale = core.GetComponent<Transform>().localScale;
+        DOTween.SetTweensCapacity(1000, 1000);
+
 
         // Subscribing to C# Event when a note plays
         ModPlayer.NoteEvent += ModPlayerOnNoteEvent;
@@ -51,18 +59,23 @@ public class MayaStuff : MonoBehaviour
             createCreature();
         }
 
-        if (newNotePlayed.main.sample == 5 && newNotePlayed.muted <= 0)
+        if (newNotePlayed.main.sample == 4 && newNotePlayed.volume == 64) 
         {
             shakeTheCore();
+            //Debug.Log(newNotePlayed.anote + " : Vol = "+newNotePlayed.volume);
         }
-        if (newNotePlayed.main.sample == 4 && newNotePlayed.muted <= 0)
+        if (newNotePlayed.main.sample == 19 && newNotePlayed.muted <= 0) 
+        {
+            makeCubes();
+        }
+        if (newNotePlayed.main.sample == 8 && newNotePlayed.muted <= 0)
         {
             spinLights();
         }        
-        if (newNotePlayed.main.sample == 17 && newNotePlayed.muted <= 0)
+        if (newNotePlayed.main.sample == 5 && newNotePlayed.muted <= 0)
         {
             flashLights();
-            Debug.Log("light flashed");
+            //Debug.Log("light flashed");
         }
 
 
@@ -89,7 +102,7 @@ public class MayaStuff : MonoBehaviour
             int bits = Random.Range(2, 4);
             for (int i = 0; i < bits; i++)
             {
-                int shapeChosen = Random.Range(0, shapes.Count);
+                int shapeChosen = Random.Range(0, 1);
                 float size = Random.Range(0.3f, 3f);
                 float spin = Random.Range(-360, 360);
 
@@ -114,19 +127,76 @@ public class MayaStuff : MonoBehaviour
 
             }
         }
+        //cube maker]
+        void makeCubes()
+        { 
+            Destroy(cubeParent, 10f);
+            //Material material = (Material) materials[Random.Range(0, materials.Length)];
+
+
+            cubeParent = new GameObject();
+            cubeLeft = new GameObject();
+            cubeRight = new GameObject();
+            
+
+            cubeParent.name = "cube parent";
+            cubeLeft.name = "left of cube";
+            cubeRight.name = "right of cube";
+
+            cubeLeft.transform.parent = cubeParent.transform;
+            cubeRight.transform.parent = cubeParent.transform;
+
+            int cubes = 10;
+            for (int i = 0; i < cubes; i++)
+            {
+                float sizeByVol = newNotePlayed.volume;
+                
+                GameObject side1 = Instantiate(shapes[2],
+                    new Vector3(Random.Range(1, 50), 0, 13f),
+                    Quaternion.identity);
+                side1.transform.localScale = new Vector3(0.1f, sizeByVol *20, 0.1f);
+                Vector3 side1Pos = side1.transform.position;
+                GameObject side2 = Instantiate(shapes[2],
+                    new Vector3(-side1Pos.x, side1Pos.y, side1Pos.z),
+                    Quaternion.identity);
+                side2.transform.localScale = new Vector3(0.1f, (sizeByVol * 20), 0.1f);
+                side1.transform.parent = cubeLeft.transform;
+                side1.transform.DOMove(new Vector3(-1000, 0, 0), 180f);
+                side2.transform.DOMove(new Vector3(1000, 0, 0), 180f);
+                cubeLeft.transform.DORotate(new Vector3(0, 0, -270), 10f);
+                cubeRight.transform.DORotate(new Vector3(0, 0, 270), 10f);
+                //side1.transform.DOScale(new Vector3(0.1f, 1, 0.1f), 2f);
+                //side2.transform.DOScale(new Vector3(0.1f, 1, 0.1f), 2f);
+                side2.transform.parent = cubeRight.transform;
+                
+                side1.SetActive(true);
+                side2.SetActive(true);
+                
+            }
+   
+
+
+
+        }
+        
+
+        //Orb shaker
         void shakeTheCore()
         {
-            float size = Random.Range(3f, 5f);
+            float size = 5f;
+            //Vector3 defaultSize = core.GetComponent<Transform>().localScale;
+           //float size = newNotePlayed.volume / 10f;
 
-            core.transform.DOScale(new Vector3(size, size, size), 0.5f).SetEase(Ease.OutBounce);
+            core.transform.DOScale(new Vector3(size, size, size), 0.75f).SetEase(Ease.OutBounce);
+            core.transform.DOScale(new Vector3(defaultCoreScale.x, defaultCoreScale.y, defaultCoreScale.z), 0.25f);
         }
-
+        //point lights
         void spinLights()
         {
             float spin = Random.Range(-360, 360);
             lightingRig.transform.DORotate(new Vector3(spin, spin, spin), 2.5f, RotateMode.Fast);
         }
-
+        //spotlights
         void flashLights()
         {
             int lightChosen = Random.Range(0, spotlights.Count);
